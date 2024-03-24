1a6e4afe4356d35669d3ac303852923cc1772e8c
Gladwell-Wanjiku <wanjeriwanjikugladwell@gmail.com>
2024-03-25 01:27:35 +0300

    commit

---
+++ src/Main.kt
@@ -0,0 +1,26 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var str = "Barnie bakes brown bagels and buns"
+    println(num);
+    val radius = 20
+    val pi = 3.14159
+    result = (radius*3)
+
+
+
+}
+fun sentence(str:String){
+    println(str.slice)
+}
+
+fun num(num1:Int, num2:Int,num3:Int,num4:Int):Int{
+    var num = arrayOf(20,34,56,12,15)
+    val sum = (num1+num2+num3+num4);
+    return(sum)
+}
+
+fun volume(radius:Int, pi:Double):Double{
+
+    println(result)
+}
